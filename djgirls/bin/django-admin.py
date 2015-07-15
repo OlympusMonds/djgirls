54cb1e82becd10e113685994443b995f65d1cbb9
Luke Mondy <luke.s.mondy@gmail.com>
2015-07-15 17:24:56 +0100

    Initial commit

---
+++ djgirls/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/luke/Dropbox/non-uni_stuff/Development/djangogirls/djgirls/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
